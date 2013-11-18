61f6737bbb051fad052f6f1906dbdf473e2682ed
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2013-11-18 03:09:20 -0200

    Modificação da Introdução, Capítulo 3, Capítulo 4 e correções ortográficas adicionais

---
+++ texto-tcc/apendices/examplejson.js
@@ -0,0 +1,44 @@
+[{
+    "id": 115771,
+    "key": "org.apache.maven:maven-compat:org.apache.maven.artifact.repository.layout.FlatRepositoryLayout",
+    "name": "FlatRepositoryLayout",
+    "scope": "FIL",
+    "qualifier": "CLA",
+    "date": "2013-11-03T00:49:39+0100",
+    "creationDate": null,
+    "lname": "org.apache.maven.artifact.repository.layout.FlatRepositoryLayout",
+    "lang": "java",
+    "msr": [{
+        "key": "ncloc",
+        "val": 51.0,
+        "frmt_val": "51"
+    }, {
+        "key": "functions",
+        "val": 6.0,
+        "frmt_val": "6"
+    }, {
+        "key": "function_complexity",
+        "val": 1.5,
+        "frmt_val": "1,5"
+    }, {
+        "key": "dit",
+        "val": 1.0,
+        "frmt_val": "1"
+    }, {
+        "key": "noc",
+        "val": 0.0,
+        "frmt_val": "0"
+    }, {
+        "key": "rfc",
+        "val": 21.0,
+        "frmt_val": "21"
+    }, {
+        "key": "lcom4",
+        "val": 2.0,
+        "frmt_val": "2,0"
+    }, {
+        "key": "ca",
+        "val": 0.0,
+        "frmt_val": "0"
+    }]
+}]
